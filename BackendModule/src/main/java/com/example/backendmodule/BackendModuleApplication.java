8f4eaedfc79f14ef312186272ab1a14340b9e56b
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-22 10:37:10 +0200

    Added BackendModule

---
+++ BackendModule/src/main/java/com/example/backendmodule/BackendModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendModuleApplication.class, args);
+    }
+
+}
---
+++ BackendModule/src/test/java/com/example/backendmodule/BackendModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
