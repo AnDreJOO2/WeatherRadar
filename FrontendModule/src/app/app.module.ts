149319bbdebd52005783befbf5ce8f4bacbe46ae
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-22 14:32:02 +0200

    Added basic components

--- FrontendModule/src/app/app.module.ts
+++ FrontendModule/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    SearcherComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
