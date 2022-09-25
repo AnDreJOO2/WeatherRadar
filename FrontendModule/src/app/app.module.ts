2a2c123d0bcec9fdae40cfe875c1fceef29b39fc
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-25 22:41:12 +0200

    Added leaflet dependencies, deleted container component.

--- FrontendModule/src/app/app.module.ts
+++ FrontendModule/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { SearcherComponent } from './components/searcher/searcher.component';
-import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     SearcherComponent,
-    ContainerComponent
   ],
   imports: [
     BrowserModule
