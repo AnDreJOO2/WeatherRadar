f4f0b0a684f5a3dabd6ef160dc63873561826e74
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-29 20:20:44 +0200

    Added @CrossOrigin in the WeatherController. Changed getWeatherForCity method to return an ResponseEntity type.

--- BackendModule/src/main/java/com/example/backendmodule/controller/WeatherController.java
+++ BackendModule/src/main/java/com/example/backendmodule/controller/WeatherController.java
@@ -2,14 +2,13 @@ package com.example.backendmodule.controller;
 
 import com.example.backendmodule.model.WeatherDto;
 import com.example.backendmodule.service.WeatherService;
-import com.example.backendmodule.weatherclient.response.WeatherServiceResponseDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class WeatherController {
 
     private final WeatherService weatherService;
@@ -19,9 +18,8 @@ public class WeatherController {
     }
 
     @GetMapping("/weather")
-    public WeatherDto getWeatherForCity(@RequestParam("city") String city){
-
-        return weatherService.getWeatherForCity(city);
+    public ResponseEntity<WeatherDto> getWeatherForCity(@RequestParam("city") String city) {
+        return new ResponseEntity<>(weatherService.getWeatherForCity(city), HttpStatus.OK);
     }
 
 
