f4c83a33c1a4efcc14571a83f9ee3fdbe27bb5d5
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-29 20:17:44 +0200

    Searcher works with backend api, added HttpClientModule.

--- FrontendModule/src/app/app.module.ts
+++ FrontendModule/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MapComponent } from './components/map/map.component';
-import { SearcherComponent } from './components/searcher/searcher.component';
+import {AppComponent} from './app.component';
+import {MapComponent} from './components/map/map.component';
+import {SearcherComponent} from './components/searcher/searcher.component';
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,11 +13,13 @@ import {FormsModule} from "@angular/forms";
     MapComponent,
     SearcherComponent,
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
