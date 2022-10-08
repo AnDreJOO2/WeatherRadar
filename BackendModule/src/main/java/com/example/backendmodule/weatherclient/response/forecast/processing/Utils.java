c4f60b8d705cb2b840c7019ea0edadd7240e3dc9
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-10-08 16:03:26 +0200

    Changes with time parsing in responseDto objects.

--- BackendModule/src/main/java/com/example/backendmodule/weatherclient/response/forecast/processing/Utils.java
+++ BackendModule/src/main/java/com/example/backendmodule/weatherclient/response/forecast/processing/Utils.java
@@ -3,12 +3,14 @@ package com.example.backendmodule.weatherclient.response.forecast.processing;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class Utils {
 
     public static String getFormattedLocalDateTime(long sunset, ZoneOffset timeZone) {
         Instant instant = Instant.ofEpochSecond(sunset);
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, timeZone);
-        return localDateTime.toString()+timeZone;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+        return localDateTime.format(formatter) + timeZone;
     }
 }
